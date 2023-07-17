1e4a1669704788464bb810064498cd1e6c2c930d
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2023-07-17 10:34:16 -0400

    adding Redux Toolkit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './Routes/home/home.component';
+import Navigation from './Routes/navigation/navigation.component';
+import Authentication from './Routes/authentication/authentication.component';
+import Shop from './Routes/shop/shop.component';
+import Checkout from './Routes/checkout/checkout.component';
+import { useDispatch } from 'react-redux';
+import { setCurrentUser } from './store/user/user.reducer';
+import { useEffect } from 'react';
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase/firebase.utils';
+
+const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user)) ;
+    });
+
+    return unsubscribe;
+  }, []);
+
+
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
