d328bd6477f5b8010148c58e7d0842b4ba50c39c
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2023-07-18 11:32:49 -0400

    Adding Stripe payment method

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,5 +1,6 @@
 import {
   BaseButton,
+  ButtonSpinner,
   GoogleSignInButton,
   InvertedButton,
 } from './button.styles';
@@ -8,6 +9,7 @@ export const BUTTON_TYPE_CLASSES = {
   base: 'base',
   google: 'google-sign-in',
   inverted: 'inverted',
+  ButtonSpinner
 };
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
@@ -17,9 +19,9 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
-  return <CustomButton {...otherProps}>{children}</CustomButton>;
+  return <CustomButton disabled = {isLoading} {...otherProps}>{isLoading ? <ButtonSpinner/> :children}</CustomButton>;
 };
 
 export default Button;
--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {SpinnerContainer} from '../spinner/spinner.styles'
 
 export const BaseButton = styled.button`
   min-width: 165px;
@@ -17,6 +18,8 @@ export const BaseButton = styled.button`
   cursor: pointer;
   display: flex;
   justify-content: center;
+  ${'' /* align-items: center; */}
+  text-align: center;
 
   &:hover {
     background-color: white;
@@ -28,6 +31,8 @@ export const BaseButton = styled.button`
 export const GoogleSignInButton = styled(BaseButton)`
   background-color: #4285f4;
   color: white;
+  text-align: center;
+  width: fit-content;
 
   &:hover {
     background-color: #357ae8;
@@ -46,3 +51,10 @@ export const InvertedButton = styled(BaseButton)`
     border: none;
   }
 `;
+
+export const ButtonSpinner = styled(SpinnerContainer)`
+width: 30px;
+height: 30px;
+
+
+`
